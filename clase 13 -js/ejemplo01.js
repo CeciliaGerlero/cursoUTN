a34344cbe4c7d9401ba2cf049744fc83a6768fda
Cecilia Gerlero <mcgerlero@gmail.com>
2024-05-22 10:32:42 -0300

    mitad clase 13

---
+++ clase 13 -js/ejemplo01.js
@@ -0,0 +1,24 @@
+/*   
+var
+let
+cosnt
+*/
+
+const nombre = 'Cecilia';
+const apellido = "Gerlero";
+const edad = 42;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+console.log(`Mi nombre es ${nombre} ${apellido} y mi edad es ${edad}`);
+
+console.log("nombre")
+document.write(nombre);
+
+
+document.write(`mi nombre es ${nombre}`)
+
+
+
---
+++ clase 13 -js/ejemplo02.js
@@ -0,0 +1,5 @@
+const nombre = prompt(`ìngrese su nombre:`, ``);
+const apellido = prompt(`ìngrese su apellido:`, ``);
+const edad = prompt(`Ingrese su edad:` ,``);
+
+console.log(`Mi nombre es ${nombre} ${apellido} y mi edad es ${edad}`);
