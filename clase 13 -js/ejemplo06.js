73a47640156672c695d1dc11b82441b784e51304
Cecilia Gerlero <mcgerlero@gmail.com>
2024-05-27 09:17:02 -0300

    ajustes lunes

--- clase 13 -js/ejemplo06.js
+++ clase 13 -js/ejemplo06.js
@@ -1,13 +1,14 @@
-const mes = parseInt(prompt(`íngrese mes del año`, ``))
+const mes = parseInt(prompt(`íngrese mes del año`, ``));
 
-if (mes == 1 || mes == 2 || mes == 3) 
+if (mes == 1 || mes == 2 || mes == 3) {
     console.log('corresponde al primer trimestre')
- else if 
-(mes == 4 || mes == 5 || mes == 6) 
+} else if (mes == 4 || mes == 5 || mes == 6) {
     console.log('corresponde al segundo trimestre')
- else if 
-(mes == 17 || mes == 8 || mes == 9) 
+} else if (mes == 7 || mes == 8 || mes == 9) {
     console.log('corresponde al tercer trimestre')
- else 
-console.log('corresponde al último trimestre')
+} else {
+    console.log('corresponde al último trimestre')
+}
 
+
+console.log(mes)
